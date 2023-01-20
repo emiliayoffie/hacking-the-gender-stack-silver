bafbbda043f69e77b775be051d173671cdb731d0
oadesiyan <108124065+oadesiyan@users.noreply.github.com>
2023-01-20 19:40:35 +0000

    new serializer feilds

--- backend/api/generate/serializers.py
+++ backend/api/generate/serializers.py
@@ -1,7 +1,13 @@
+import re
 from rest_framework.serializers import Serializer
 from rest_framework.serializers import ModelSerializer
-from rest_framework.fields import CharField
+from rest_framework.fields import CharField, ListField, IntegerField
 
 
 class SmilesInputSerializer(Serializer):
     smiles = CharField(required=True)
+    rGroup = ListField(required=True)
+    rGroup_Labels = ListField(required=True)
+    numGen = IntegerField(required=True)
+    numMutations = IntegerField(required=True)
+    numParents = IntegerField(required=True)
